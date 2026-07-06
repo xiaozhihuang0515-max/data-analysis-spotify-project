264ed8bb95b359f8548676d4229d66165d300752
xiaozhihuang0515-max <xiaozhihuang0515@gmail.com>
2026-07-06 22:27:59 +0800

    Update data_loader.py

--- 2_src/data_loader.py
+++ 2_src/data_loader.py
@@ -3,9 +3,13 @@ import pandas as pd
 
 class DataLoader:
 
-    def __init__(self, path="app/data/spotify.csv"):
+    def __init__(self, path):
         self.path = path
 
     def load(self):
         df = pd.read_csv(self.path)
+
+        # basic cleanup
+        df = df.dropna()
+
         return df
